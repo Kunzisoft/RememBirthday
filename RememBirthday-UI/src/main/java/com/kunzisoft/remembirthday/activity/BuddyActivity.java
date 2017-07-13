e4f08d1da98742b129f67202cb89d486d3017254
J-Jamet <jamet.jeremy@outlook.com>
2017-07-14 01:47:35 +0200

    Manage and solve bugs for styles

--- RememBirthday-UI/src/main/java/com/kunzisoft/remembirthday/activity/BuddyActivity.java
+++ RememBirthday-UI/src/main/java/com/kunzisoft/remembirthday/activity/BuddyActivity.java
@@ -77,9 +77,6 @@ public class BuddyActivity extends AbstractBuddyActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
